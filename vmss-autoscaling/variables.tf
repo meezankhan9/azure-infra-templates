ff64bc2e19f09d2f40e1ef19d4e0b92f1fab378b
Meezan Khan <73093094+meezankhan9@users.noreply.github.com>
2025-05-26 16:37:33 +0530

    Update variables.tf

--- vmss-autoscaling/variables.tf
+++ vmss-autoscaling/variables.tf
@@ -1,1 +1,17 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "prefix" {
+  description = "Prefix used for naming all resources"
+  type        = string
+  default     = "vmssdemo"
+}
 
